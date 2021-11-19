a94c2f48f67703213704ed7b61ece6b41a56ca53
Paulo Rodrigo <paulorodrigo1992@gmail.com>
2021-11-18 23:32:27 -0300

    BufferedWriter and FileWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,6 @@
 package application;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
@@ -10,21 +8,18 @@ public class Program {
 
     public static void main(String[] args) {
 
-        File file = new File("/home/paulo/IdeaProjects/text");
-        Scanner sc = null;
-        try{
-            sc = new Scanner(file);
-            while(sc.hasNextLine()){
-                System.out.println(sc.nextLine());
+        String[] lines = new String [] {"Good morning", "Good afternoon", "Good night"};
+
+        String path = "/home/paulo/IdeaProjects/texto";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            for (String line : lines){
+                bw.write(line);
+                bw.newLine();
             }
         }
-        catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            if (sc != null){
-                sc.close();
-            }
+        catch (IOException e){
+            e.printStackTrace();
         }
 
     }
